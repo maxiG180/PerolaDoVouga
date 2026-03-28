f11ecd81caab712e7ea3cbf1794e32a2aac08af4
Maksym Grebeniuk <ytmax180@gmail.com>
2026-03-28 10:42:51 +0100

    logo

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -110,7 +110,7 @@ export default async function RootLayout({
     return (
         <html lang={locale} className={`${playfair.variable} ${inter.variable}`}>
             <head>
-                <link rel="apple-touch-icon" href="/icon-192.png" />
+                <link rel="apple-touch-icon" href="/logo.png" />
                 <meta name="apple-mobile-web-app-capable" content="yes" />
                 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
                 <meta name="mobile-web-app-capable" content="yes" />
